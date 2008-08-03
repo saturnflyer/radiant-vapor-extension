ebb5db81bfca9e2e7e6ec537f11987150b686a7b
Jim Gay <jim@saturnflyer.com>
2008-08-02 23:41:35 -0400

    controller specs added

---
+++ spec/controllers/admin/flow_meters_controller_spec.rb
@@ -0,0 +1,78 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Admin::FlowMetersController do
+  scenario :users
+  
+  before(:each) do
+    login_as :developer
+  end
+  
+  describe "/admin/flow_meters with GET" do
+    before(:each) do
+      @flow_meter = mock_model(FlowMeter)
+      @new_flow_meter = mock_model(FlowMeter)
+      FlowMeter.stub!(:find).with(:all).and_return([@flow_meter])
+      FlowMeter.stub!(:new).and_return(@new_flow_meter)
+      get :index
+    end
+    
+    it "should render the admin/index template" do
+      response.should render_template('index')
+    end
+    
+    it "should get all flow_meters" do
+      assigns[:flow_meters].should == [@flow_meter]
+    end
+    
+    it "should get a new flow_meter" do
+      assigns[:flow_meter].should == @new_flow_meter
+    end
+  end
+  describe "/admin/flow_meters with valid POST" do
+    before(:each) do
+      @flow_meter = FlowMeter.create!(:catch_url => 'this', :redirect_url => 'that', :status => '302 Found')
+      @flow_meter_count = FlowMeter.count
+      post :create, :flow_meter => {:catch_url => 'other', :redirect_url => 'that'}
+    end
+    
+    it "should increase the flow_meter_count by 1" do
+      FlowMeter.count.should > @flow_meter_count
+    end
+    
+    it "should redirect to the index" do
+      response.should redirect_to('/admin/flow_meters')
+    end
+  end
+  describe "/admin/flow_meters with invalid POST" do
+    before(:each) do
+      post :create, :flow_meter => {:catch_url => 'admin', :redirect_url => 'that'}
+    end
+    
+    it "should assign the error message to the flash" do
+      flash[:error].should_not be_blank
+    end
+    
+    it "should redirect to the index" do
+      response.should redirect_to('/admin/flow_meters')
+    end
+  end
+  describe "/admin/flow_meters/:id with DELETE" do
+    before(:each) do
+      @flow_meter = FlowMeter.create!(:id => 1, :catch_url => 'this', :redirect_url => 'that', :status => '302 Found')
+      @flow_meter_count = FlowMeter.count
+    end
+    
+    it "should find the flow_meter" do
+      FlowMeter.should_receive(:find).with(@flow_meter.id.to_s).and_return(@flow_meter)
+      delete :destroy, :id => @flow_meter.id
+    end
+    it "should reduce the flow_meter count by 1" do
+      delete :destroy, :id => @flow_meter.id
+      FlowMeter.count.should < @flow_meter_count
+    end
+    it "should redirect to the index" do
+      delete :destroy, :id => @flow_meter.id
+      response.should redirect_to('/admin/flow_meters')
+    end
+  end
+end
