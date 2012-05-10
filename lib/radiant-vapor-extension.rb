04b58d89692c3160f5f91b4e7088bbfd6fa5ffb5
Jim Gay <jim@saturnflyer.com>
2012-05-10 00:21:39 -0400

    allow redirect to https

--- lib/radiant-vapor-extension.rb
+++ lib/radiant-vapor-extension.rb
@@ -1,5 +1,5 @@
 module RadiantVaporExtension
-  VERSION     = "2.1.3"
+  VERSION     = "2.1.4"
   SUMMARY     = "User managed redirects."
   DESCRIPTION = "User managed redirects."
   URL         = "http://saturnflyer.com/"
